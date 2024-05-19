a2a5ca852e98dfaab81c310de3cdbf014370faf2
alehdez0809 <ahernandezq2101@alumno.ipn.mx>
2024-05-18 21:02:36 -0600

    cambios en el config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const MYSQLHOST= 'viaduct.proxy.rlwy.net'
-const MYSQLPASSWORD= 'ETIHuTaULQMlxhKFVFniiHbWuaeMlZQO'
-const MYSQLPORT= '27509'
-const MYSQLUSER= 'root'
-const MYSQLDATABASE= 'railway'
+const MYSQLHOST= 'viaduct.proxy.rlwy.net';
+const MYSQLPASSWORD= 'ETIHuTaULQMlxhKFVFniiHbWuaeMlZQO';
+const MYSQLPORT= '27509';
+const MYSQLUSER= 'root';
+const MYSQLDATABASE= 'railway';
 
 export const metodos = {
     MYSQLHOST,
